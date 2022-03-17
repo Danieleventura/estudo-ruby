ac0097d979de94e88f7afa5afcbd00d31ea1c068
Daniele Ventura <danieleuepb@hotmail.com>
2022-03-17 10:50:36 -0300

    aula captura de funcao

---
+++ introducao-Funcoes-e-metodos/Captura-Funcao/BemVindo.rb
@@ -0,0 +1,9 @@
+puts('Entre com o seu nome: ')
+nome = gets() #Define uma variavel e guarda o valor informado nela
+puts("Bem-vindo #{nome}")
+
+#Diferença de print e puts
+=begin
+     print -> comando que não quebra linha
+     puts -> comando que irá quebrar linha
+=end
