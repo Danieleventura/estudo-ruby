9ff6e898859c6a55801d8f7c60f8d9b9f2e6b06a
Daniele Ventura <danieleuepb@hotmail.com>
2022-03-31 12:33:54 -0300

    aula metodo objeto

---
+++ Classes-Objetos/Usina.rb
@@ -0,0 +1,10 @@
+class UsinaAngra
+
+    def ligar_reator(cidade, hora, dia, capacidade)
+        print("Abastecimento da cidade #{cidade} na hora #{hora} do dia #{dia} com capacidade de #{capacidade}")
+
+    end
+end
+
+usina = UsinaAngra.new
+usina.ligar_reator("Campina Grande", 8.30,25, "1mw" )
