02c5659fb774bd22a5b5667c86fc843fa9a233e4
Daniele Ventura <danieleuepb@hotmail.com>
2022-04-01 13:16:41 -0300

    aula arrays formas corretas

---
+++ Arrays/Correto.rb
@@ -0,0 +1,9 @@
+umarray = Array.new(['a','b'])
+
+umarray1 = Array.new( ['a','b','c'] )
+
+umarray2 = Array.new ['a', 'b', 'c', 'd']
+
+print("Array: #{umarray}\n")
+print("Array1: #{umarray1}\n")
+print("Array2: #{umarray2}\n")
