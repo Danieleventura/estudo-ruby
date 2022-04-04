e2f8ae41684f9db9f70bf6e07f6324416bca7832
Daniele Ventura <danieleuepb@hotmail.com>
2022-04-04 10:17:28 -0300

    aula Array ordem

---
+++ Arrays/Controle.rb
@@ -0,0 +1,21 @@
+#Array com 7 valores
+texto = ['s','e','u',' ', 'n', 'o','m','e']
+
+#Nós podemos imprimir na ordem
+print('Primeiro indice ao terceiro: ', texto[0,3])
+puts("Primeiro indice ao terceiro: #{texto[0,3]}")
+puts
+print('Ultimo indice menos 3: ', texto[-3, 7])
+puts
+print('Um calculo: ', texto[4-8])
+puts
+print('Primeiro valor ate o limite: ', texto[0..7])
+puts 
+print('Indexado: ', texto[-5..7])
+
+=begin
+    Podemos especificar um começo e um fim
+    texto[<Começo do indice>, <número de itens>]
+    Nós também podemos especificar uma faixa
+    texto[<Começo do indice>..<Fim di indice>]
+=end
