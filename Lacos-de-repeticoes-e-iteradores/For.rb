3aef4d081887c8a389b68ab5ae7244be607f51e7
Daniele Ventura <danieleuepb@hotmail.com>
2022-04-05 10:41:26 -0300

    aula laço de repeteição For

---
+++ Lacos-de-repeticoes-e-iteradores/For.rb
@@ -0,0 +1,19 @@
+puts
+puts("Primeira forma")
+
+for x in [1,2,3,4] do
+    puts(x)
+end
+
+puts
+puts("Segunda forma")
+#'do' é opcional quando usado em uma linha multipla
+for a in['a',2,'dani',5]
+    puts(a)
+end
+
+puts
+puts("Terceira forma")
+# 'do' é obrigatorio quando usado em linha unica
+for i in [1,2,4,78] do puts(i) end
+
