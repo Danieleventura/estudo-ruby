4617ee51afb82e24ce8ad9d7d8e7b88cc66bacd0
Daniele Ventura <danieleuepb@hotmail.com>
2022-04-05 14:01:40 -0300

    aula Module

---
+++ Module-Mixins/Modulo.rb
@@ -0,0 +1,21 @@
+module Tanque
+    #Constantes - letras maiusculas
+    DISPARAR = "fogo!"
+
+    RECARREGAR = "carregando"
+
+    #metodo de instância
+    def movimento
+        return "movimentando e disparando #{DISPARAR}"
+    end
+
+    #metodo de modulo
+    def Tanque.movimento
+        return "movimento"
+    end
+
+end 
+
+puts(Tanque::DISPARAR)
+puts(Tanque::RECARREGAR)
+puts(Tanque.movimento)
